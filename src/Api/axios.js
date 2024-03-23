7018a5d68a959d5362b9f65f503508154ae0e8b9
yordanos legesse <yordanoslegesse15@gmail.com>
2024-03-23 16:29:53 +0300

    baseURL changed

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: "http://127.0.0.1:5001/clone-6a383/us-central1/api",
+	// baseURL: "http://127.0.0.1:5001/clone-6a383/us-central1/api",
+	baseURL: "https://e-commerce-api-1.onrender.com/" , 
 });
 
 
